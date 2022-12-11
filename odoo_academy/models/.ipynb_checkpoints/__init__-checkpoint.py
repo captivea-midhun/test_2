32d7dadac41d0c71bb1e4ef6e1f9c49072371459
Midhun <midhun.raj@captivea.us>
2022-12-11 16:10:19 +0000

    Testing

--- odoo_academy/models/.ipynb_checkpoints/__init__-checkpoint.py
+++ odoo_academy/models/.ipynb_checkpoints/__init__-checkpoint.py
@@ -1,20 +1,3 @@
 # -*- coding: utf-8 -*-
 
-# Import existing python modules created by Odoo
-from odoo import models, fields, api      # You usually want to import these three
-
-class Course(models.Model):     # Inherit abstract model: models.Model
-    
-    # Define model attributes
-    _name = 'academy.course'
-    _description = 'Course Info'
-    
-    # Initialize fields
-    name = fields.Char(string='Title', required=True)
-    description = fields.Text(string='Description')
-    level = fields.Selection(string='Level2',
-                             selection= [('beginner', 'Beginner'),
-                                         ('intermediate', 'Inermediate'),
-                                         ('advanced', 'Advanced')],
-                            copy=False)  
-    test = fields.Char(string='Test', required=False)
+from . import course
